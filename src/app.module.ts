40919a775dfba3fafe47189b70cd0d6b27288476
Hanna Park <swucs7@gmail.com>
2023-12-29 18:16:11 +0900

    auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeORMConfig';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
   controllers: [],
   providers: [],
 })
